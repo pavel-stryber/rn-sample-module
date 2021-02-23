7d794b280352e0dfec2db7c75c0fa9f537060ed5
Pavel Lapin <pavel@stryber.com>
2021-02-23 11:22:29 +0200

    initial commit

---
+++ ios/RNSampleModule/RNSampleModule.h
@@ -0,0 +1,12 @@
+//
+//  RNSampleModule.h
+//  RNSampleModule
+//
+//  Created by pavel lapin on 2/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNSampleModule : NSObject
+
+@end
---
+++ ios/RNSampleModule/RNSampleModule.m
@@ -0,0 +1,12 @@
+//
+//  RNSampleModule.m
+//  RNSampleModule
+//
+//  Created by pavel lapin on 2/26/21.
+//
+
+#import "RNSampleModule.h"
+
+@implementation RNSampleModule
+
+@end
